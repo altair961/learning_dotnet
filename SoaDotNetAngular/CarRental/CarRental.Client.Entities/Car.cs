598352c35ddbdb5e2d2deff9468b155cdb8968c5
altair961 <altair961@github.com>
2017-08-25 22:48:18 +0200

    Added properties to client Car entity

--- SoaDotNetAngular/CarRental/CarRental.Client.Entities/Car.cs
+++ SoaDotNetAngular/CarRental/CarRental.Client.Entities/Car.cs
@@ -2,17 +2,82 @@
 {
     public class Car
     {
-        int _CarId;
+        private int carId;
+        private string description;
+        private string color;
+        private int year;
+        private decimal rentalPrice;
+        private bool currentlyRented;
 
         public int CarId
         {
             get
             {
-                return _CarId;
+                return carId;
             }
             set
             {
-                _CarId = value;
+                carId = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+
+        public decimal RentalPrice
+        {
+            get
+            {
+                return rentalPrice;
+            }
+            set
+            {
+                rentalPrice = value;
+            }
+        }
+
+        public bool CurrentlyRented
+        {
+            get
+            {
+                return currentlyRented;
+            }
+            set
+            {
+                currentlyRented = value;
             }
         }
     }
