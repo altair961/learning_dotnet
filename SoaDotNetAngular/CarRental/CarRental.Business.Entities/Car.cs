7fd44d7ffb0f6b11b02e7d45be7783b7b96c4aeb
altair961 <altair961@github.com>
2017-08-25 19:33:16 +0200

    Added properties to business Car entity

--- SoaDotNetAngular/CarRental/CarRental.Business.Entities/Car.cs
+++ SoaDotNetAngular/CarRental/CarRental.Business.Entities/Car.cs
@@ -2,5 +2,11 @@
 {
     public class Car
     {
+        public int CarId { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public decimal RentalPrice { get; set; }
+        public bool CurrentlyRented { get; set; }
     }
 }
