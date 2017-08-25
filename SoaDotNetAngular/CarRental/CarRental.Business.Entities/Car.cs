4a67c68836d765ef4d74593f82382738b3780d96
altair961 <altair961@github.com>
2017-08-25 19:37:32 +0200

    Added DataContract and DataMember attributes in order to enable serialization for Car class and its members

--- SoaDotNetAngular/CarRental/CarRental.Business.Entities/Car.cs
+++ SoaDotNetAngular/CarRental/CarRental.Business.Entities/Car.cs
@@ -1,12 +1,26 @@
-﻿namespace CarRental.Business.Entities
+﻿using System.Runtime.Serialization;
+
+namespace CarRental.Business.Entities
 {
+    [DataContract]
     public class Car
     {
+        [DataMember]
         public int CarId { get; set; }
+
+        [DataMember]
         public string Description { get; set; }
+
+        [DataMember]
         public string Color { get; set; }
+
+        [DataMember]
         public int Year { get; set; }
+
+        [DataMember]
         public decimal RentalPrice { get; set; }
+
+        [DataMember]
         public bool CurrentlyRented { get; set; }
     }
 }
