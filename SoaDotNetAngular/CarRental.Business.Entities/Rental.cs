005ebc3bddab195bb986287ae91cc78a61fc76ef
altair961 <altair961@github.com>
2017-08-26 17:58:22 +0200

    Added Rental business entity the same way as the Car entity

---
+++ SoaDotNetAngular/CarRental.Business.Entities/Rental.cs
@@ -0,0 +1,35 @@
+﻿using Core.Common;
+using Core.Common.Contracts;
+using System;
+using System.Runtime.Serialization;
+
+namespace CarRental.Business.Entities
+{
+    [DataContract]
+    public class Rental : EntityBase, IIdentifiableEntity
+    {
+        [DataMember]
+        public int RentalId { get; set; }
+
+        [DataMember]
+        public int AccountId { get; set; }
+
+        [DataMember]
+        public int CarId { get; set; }
+
+        [DataMember]
+        public DateTime DateRented { get; set; }
+
+        [DataMember]
+        public DateTime DateDue { get; set; }
+
+        [DataMember]
+        public DateTime? DateReturned { get; set; }
+
+        public int EntityId
+        {
+            get { return RentalId; }
+            set { RentalId = value; }
+        }
+    }
+}
