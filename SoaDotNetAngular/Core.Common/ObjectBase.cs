b286ac9b00e43f0d887cb34a477367045c2041a6
altair961 <altair961@github.com>
2017-08-27 11:19:32 +0200

    Added ObjectBase class

---
+++ SoaDotNetAngular/Core.Common/ObjectBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Core.Common
+{
+    public class ObjectBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
