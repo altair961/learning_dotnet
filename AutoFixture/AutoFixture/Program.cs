0aa3fbfae92f4eb6d2dc6fe383f1574987ca34bb
altair961 <altair961@github.com>
2017-09-10 10:46:53 +0200

    Added DemoCode.Tests project and removed AutoFixture project which was not included into the solution

--- AutoFixture/AutoFixture/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutoFixture
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
--- AutoFixture/DemoCode.Tests/Properties/AssemblyInfo.cs
+++ AutoFixture/DemoCode.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutoFixture")]
+[assembly: AssemblyTitle("DemoCode.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AutoFixture")]
+[assembly: AssemblyProduct("DemoCode.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5e5f8fab-5ab5-445f-9f8d-71fb7c1b3574")]
+[assembly: Guid("53e24e94-3d26-4cee-ae25-c5f459840e98")]
 
 // Version information for an assembly consists of the following four values:
 //
