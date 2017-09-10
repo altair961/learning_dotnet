400010b0c1e841b8f233ba9e742b71d2d9a94f88
altair961 <altair961@github.com>
2017-09-10 17:59:51 +0200

    Added Decimals test

---
+++ AutoFixture/DemoCode.Tests/Numbers.cs
@@ -0,0 +1,39 @@
+﻿using Ploeh.AutoFixture;
+using Xunit;
+
+namespace DemoCode.Tests
+{
+    public class Numbers
+    {
+        [Fact]
+        public void Ints()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var sut = new IntCalculator();
+            int num = fixture.Create<int>();
+
+            // Act
+            sut.Add(num);
+
+            // Assert
+            Assert.Equal(num, sut.Value);
+        }
+
+        [Fact]
+        public void Decimals()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var sut = new DecimalCalculator();
+
+            decimal num = fixture.Create<decimal>();
+
+            // Act
+            sut.Add(num);
+
+            // Assert
+            Assert.Equal(num, sut.Value);
+        }
+    }
+}
