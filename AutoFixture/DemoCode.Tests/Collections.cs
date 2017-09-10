b545dcc1a2b304951b513772f19dba6c66f8d74b
altair961 <altair961@github.com>
2017-09-10 18:29:07 +0200

    Added SequenceOfStrings test

---
+++ AutoFixture/DemoCode.Tests/Collections.cs
@@ -0,0 +1,23 @@
+﻿using Ploeh.AutoFixture;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xunit;
+
+namespace DemoCode.Tests
+{
+    public class Collections
+    {
+        [Fact]
+        public void SequenceOfStrings()
+        {
+            var fixture = new Fixture();
+
+            IEnumerable<string> messages = fixture.CreateMany<string>();
+
+            foreach (var message in messages)
+            {
+                Debug.WriteLine(message);   
+            }
+        }
+    }
+}
