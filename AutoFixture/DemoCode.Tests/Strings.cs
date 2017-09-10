f550f3a48c4f13d8bb89274e5bf280025e51c294
altair961 <altair961@github.com>
2017-09-10 17:37:29 +0200

    Added BasicString test

---
+++ AutoFixture/DemoCode.Tests/Strings.cs
@@ -0,0 +1,25 @@
+﻿using Ploeh.AutoFixture;
+using Xunit;
+
+namespace DemoCode.Tests
+{
+    public class Strings
+    {
+        [Fact]
+        public void BasicString()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var sut = new NameJoiner();
+
+            var firstName = fixture.Create<string>();
+            var lastName = fixture.Create<string>();
+
+            // Act
+            var result = sut.Join(firstName, lastName);
+
+            // Assert
+            Assert.Equal(firstName + ' ' + lastName, result);
+        }
+    }
+}
