f245987f159bd885fb8296d03315ed424732a294
altair961 <altair961@github.com>
2017-09-10 18:18:53 +0200

    Added DateTimes test

---
+++ AutoFixture/DemoCode.Tests/DatesAndTimes.cs
@@ -0,0 +1,23 @@
+﻿using Ploeh.AutoFixture;
+using System;
+using Xunit;
+
+namespace DemoCode.Tests
+{
+    public class DatesAndTimes
+    {
+        [Fact]
+        public void DateTimes()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            DateTime logTime = fixture.Create<DateTime>();
+
+            // Act
+            LogMessage result = LogMessageCreator.Create(fixture.Create<string>(), logTime);
+
+            // Assert
+            Assert.Equal(logTime.Year, result.Year);
+        }
+    }
+}
