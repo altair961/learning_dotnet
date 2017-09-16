1cefbaee77f174719a1d9b948131be78aef786a0
altair961 <altair961@github.com>
2017-09-16 16:59:40 +0200

    Enabled ObjectBase to keep track of propertyChanged event subscribers avoiding double wiring to it

--- SoaDotNetAngular/Core.Common/ObjectBase.cs
+++ SoaDotNetAngular/Core.Common/ObjectBase.cs
@@ -1,21 +1,52 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Core.Common
 {
     public class ObjectBase : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        private event PropertyChangedEventHandler propertyChanged;
+
+        List<PropertyChangedEventHandler> propertyChangedSubsribers =
+            new List<PropertyChangedEventHandler>();
 
         protected virtual void OnPropertyChanged(string propertyName)
         {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            OnPropertyChanged(propertyName, true);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add
+            {
+                if(!propertyChangedSubsribers.Contains(value))
+                {
+                    propertyChanged += value;
+                    propertyChangedSubsribers.Add(value);
+                }
+            }
+            remove
+            {
+                propertyChanged -= value;
+                propertyChangedSubsribers.Remove(value);
+            }
+        }
+
+
+        protected virtual void OnPropertyChanged(string propertyName, bool makeDirty)
+        {
+            if (propertyChanged != null)
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+            if (makeDirty)
+                IsDirty = true;
         }
 
         private bool isDirty;
         public bool IsDirty
         {
             get { return isDirty; }
+            private set { isDirty = value; }
         }
     }
 }
