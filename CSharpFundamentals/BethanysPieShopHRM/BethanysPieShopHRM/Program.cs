9de97b890be1224f522855c6c40aa1227aea0f3e
altair961 <altair89@list.ru>
2022-05-08 18:33:46 +0300

    Examples of some primitive types have been added

--- CSharpFundamentals/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ CSharpFundamentals/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+
+Console.WriteLine("Please enter your name: ");
+
+string firstName = Console.ReadLine();
+
+int monthlyWage = 1234;
+
+int months = 12, bonus = 1000;
+
+bool isActive = true;
+
+double rating = 99.25;
+
+int hoursWorked = 125;
+
+hoursWorked = 148;
