eb44bbd4a232d6d0e2c539889b35d7d7365ea623
altair961 <altair89@list.ru>
2022-05-22 16:52:11 +0300

    Added Validate, Retrieve and Save methods.

--- ObjectOrientedProgrammingFundamentalsInCSharp/ACM/ACM.BL/Customer.cs
+++ ObjectOrientedProgrammingFundamentalsInCSharp/ACM/ACM.BL/Customer.cs
@@ -31,5 +31,30 @@ namespace ACM.BL
         }
 
         public static int InstanceCount { get; set; }
+
+        public bool Validate()
+        {
+            var isValid = true;
+            
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+        }
+
+        public Customer Retrieve(int customerId)
+        {
+            return new Customer();
+        }
+
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
     }
 }
