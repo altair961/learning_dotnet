6e6aa7cd5b16bcc83517c6969b0170058f526c98
altair961 <altair89@list.ru>
2022-06-09 09:08:12 +0300

    Adding Knockoutjs through nuget package was demonstrated

---
+++ KnockoutFundamentals/VisualStudioSetupForNockout/VisualStudioSetupForNockout/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VisualStudioSetupForNockout.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
